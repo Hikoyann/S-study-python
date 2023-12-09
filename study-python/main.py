41d3e370cc62a7f4394c6d9bbb7732b3b2064bb4
Hikoyann <i21i022dl@ous.jp>
2023-12-09 19:35:16 +0900

    クラスの基本を学習

--- study-python/main.py
+++ study-python/main.py
@@ -1,1 +1,21 @@
-print("test")
+class Person(object):
+  def __init__(self, name):
+    self.name = name
+    print("First")
+    print("{}".format(self.name))
+    self.run(2)
+
+  def run(self, num):
+    print("run\n" * num)
+
+  def say_something(self):
+    print("hello")
+
+# person = Person()
+person = Person("AA")
+# person.say_something()
+
+# def person(name):
+#   if name == "a":
+#     print("a")
+
