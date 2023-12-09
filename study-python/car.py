1a6a1dc2595eecd04a6af1c383141d95e57d0c21
Hikoyann <i21i022dl@ous.jp>
2023-12-09 20:03:36 +0900

    クラスの初期化、メソッドの引数に値を定義

--- study-python/car.py
+++ study-python/car.py
@@ -1,25 +1,29 @@
 class Car(object):
+  def __init__(self, model=None):
+    self.model = model
   def run(self):
     print("run")
 
+class ToyotaCar(Car):
+  pass
+
 class TeslaCar(Car):
+  def run(self):
+    print("run fast")
   def auto_run(self):
     print("auto run")
 
-class ToyotaCar(TeslaCar):
-  pass
 
 car = Car()
 car.run()
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar("ssss")
+print(toyota_car.model)
 toyota_car.run()
 
 tesla_car = TeslaCar()
 tesla_car.run()
 tesla_car.auto_run()
 
-print("aaa")
 
-toyota_car.auto_run()
 
 
