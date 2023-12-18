0d1283f30d22abbd8eb017d6d68dc77e79babc8f
Hikoyann <i21i022dl@ous.jp>
2023-12-18 21:11:07 +0900

    複数のクラスを使ったプログラム・doctestの勉強

---
+++ study-python/test.py
@@ -0,0 +1,18 @@
+class Cal(object):
+  def add_num_and_double(self, x, y):
+    """
+    Add and double
+    >>> c = Cal()
+    >>> c.add_num_and_double(1,1)
+    4
+
+    >>> c.add_num_and_double("1","1")
+    '1111'
+    """
+    result = x + y
+    result *= 2
+    return result
+  
+if __name__ == "__main__":
+  import doctest
+  doctest.testmod()
