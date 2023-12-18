9bc47d65ac7ddbe57cac28e1e47a857f6a4c8e37
Hikoyann <i21i022dl@ous.jp>
2023-12-18 23:57:49 +0900

    テストコードの書き方を変更

--- study-python/test_unit.py
+++ study-python/test_unit.py
@@ -4,11 +4,14 @@ import test
 class CalTest(unittest.TestCase):
     def test_add_num_and_double(self):
         cal = test.Cal()
-        self.assertEqual(cal.add_num_and_double(1, 1), 4)
+        self.assertEqual(cal.add_num_and_double(1, 1), 5)
 
     def test_add_num_and_double_raise(self):
         cal = test.Cal()
         with self.assertRaises(ValueError):
             cal.add_num_and_double("1", "1")
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+# unittest.main()
+# python -m unittest test_unit.py
